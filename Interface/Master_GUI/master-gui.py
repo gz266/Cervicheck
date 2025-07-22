c7793b6146fe4cbd47b0afc2ba0f2dfcf4e98bd4
Neanth Suresh <ns976@cornell.edu>
2025-07-22 11:40:34 -0400

    Update master-gui.py

--- Interface/Master_GUI/master-gui.py
+++ Interface/Master_GUI/master-gui.py
@@ -88,7 +88,7 @@ def pressureSweep():
 
 def changeSweepSettings():
     maxPres = int(presStart.get()) + int(presIncr.get()) * int(presNumIncr.get())
-    if int(presStart.get() > 0):
+    if int(presStart.get()) > 0:
         raise Pressure("Pressure must begin at 0 kPa or less")
     elif maxPres < -50:
         raise Pressure("Pressure must be under 50 kPa")
