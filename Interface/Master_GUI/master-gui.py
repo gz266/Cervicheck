1b45191135a22d1725f4c046630f387955d62943
gz266 <gz266@cornell.edu>
2025-07-29 10:35:18 -0400

    Update master-gui.py

--- Interface/Master_GUI/master-gui.py
+++ Interface/Master_GUI/master-gui.py
@@ -18,7 +18,7 @@ import matplotlib.animation as animation
 matplotlib.use('agg')
 
 
-commPort = '/dev/cu.usbmodem1401'
+commPort = '/dev/cu.usbmodem21301'
 ser = serial.Serial(commPort, baudrate = 9600)
 sleep(2)
 
