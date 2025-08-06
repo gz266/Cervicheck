862f56b4c1f6aaf80caebb4e2d85e1d380e206fd
gz266 <gz266@cornell.edu>
2025-08-06 12:46:26 -0400

    mux

---
+++ Interface/Impedance_8Mux/Impedance_8Mux.ino
@@ -0,0 +1,147 @@
+/*
+ad5933-test
+    Reads impedance values from the AD5933 over I2C and prints them serially.
+*/
+
+#include <Wire.h>
+#include "AD5933.h"
+
+int start_freq = 10000;
+int freq_incr = 5000;
+int num_incr = 5;
+int ref_resist = 270;
+
+char userInput;
+String data;
+
+double gain[20];
+double phase[20];
+double phaseRef[20];
+
+int sL[3] = { 8, 9, 10 };
+int MUXtable[8][3] = { { 1, 0, 1 }, { 1, 1, 0 }, { 0, 0, 0 }, { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 }, { 0, 1, 1 }, { 1, 1, 1 } };
+
+int curPad = 1;
+
+void setup(void) {
+  // Begin I2C
+  Wire.begin();
+
+  pinMode(A2, INPUT);
+
+  // Begin serial at 9600 baud for output
+  Serial.begin(9600);
+  Serial.println("AD5933 Test Started!");
+  
+  // Perform initial configuration. Fail if any one of these fail.
+  if (!(AD5933::reset() && AD5933::setInternalClock(true) && AD5933::setStartFrequency(start_freq) && AD5933::setIncrementFrequency(freq_incr) && AD5933::setNumberIncrements(num_incr) && AD5933::setPGAGain(PGA_GAIN_X1))) {
+    Serial.println("FAILED in initialization!");
+    while (true)
+      ;
+  }
+  if (!AD5933::calibrate(gain, phaseRef, ref_resist, num_incr + 1)) {
+    Serial.println("Calibration failed...");
+    while (true);
+  }
+  Serial.println("Calibrated!");
+  
+}
+
+void loop(void) {
+
+  if(Serial.available()>0){
+    userInput = Serial.read();   
+    if(userInput == 'o'){
+      data = Serial.readStringUntil('\r');
+      start_freq = data.toInt();
+      data = Serial.readStringUntil('\r');
+      freq_incr = data.toInt();
+      data = Serial.readStringUntil('\r');
+      num_incr = data.toInt();
+      data = Serial.readStringUntil('\r');
+      ref_resist = data.toInt();
+      calibrateAD5933(start_freq, freq_incr, num_incr, ref_resist);
+    }
+    if(userInput == 'p'){
+      Serial.println("Performing impedance sweep...");
+      frequencySweepEasy();
+    }
+    
+  }
+}
+
+// Easy way to do a frequency sweep. Does an entire frequency sweep at once and
+// stores the data into arrays for processing afterwards. This is easy-to-use,
+// but doesn't allow you to process data in real time.
+void frequencySweepEasy() {
+  // Create arrays to hold the data
+  int real[num_incr + 1], imag[num_incr + 1];
+
+  // Perform the frequency sweep
+  if (AD5933::frequencySweep(real, imag, num_incr + 1)) {
+    // Print the frequency data
+    int cfreq = start_freq / 1000;
+    for (int i = 0; i < num_incr + 1; i++, cfreq += freq_incr / 1000) {
+      // Print raw frequency data
+      Serial.print(cfreq);
+      Serial.print(": Impedance = ");
+      // Serial.print(real[i]);
+      // Serial.print("/I=");
+      // Serial.print(imag[i]);
+
+      // Compute impedance
+      // phase[i] phase from the board
+      double phase_new = (int)(atan2(imag[i], real[i]) * (180.0 / M_PI)); // Convert to degrees
+      
+      double magnitude = sqrt(pow(real[i], 2) + pow(imag[i], 2));
+      double impedance = 1 / (magnitude * gain[i]);
+      phase[i] = atan2(imag[i], real[i]) * (180.0 / M_PI) - phaseRef[i]; // Convert to degrees
+
+
+      // Serial.print("  |Z|=");
+      Serial.println(impedance);
+      
+      Serial.print("R: ");
+      Serial.println(real[i],10);
+      Serial.print("I: ");
+      Serial.println(imag[i],10);
+      Serial.print("Mag: ");
+      Serial.println(magnitude,10);
+      Serial.print("Gain: ");
+      Serial.println(gain[i],10);
+      Serial.print("Phase: ");
+      Serial.println(phase_new,10);
+
+
+    }
+    Serial.println("Frequency sweep complete!");
+  } else {
+    Serial.println("Frequency sweep failed...");
+  }
+}
+
+void calibrateAD5933(int start_freq, int freq_incr, int num_incr, int reference) {
+  
+  if (!(AD5933::reset() && AD5933::setInternalClock(true) && AD5933::setStartFrequency(start_freq) && AD5933::setIncrementFrequency(freq_incr) && AD5933::setNumberIncrements(num_incr) && AD5933::setPGAGain(PGA_GAIN_X1))) {
+    Serial.println("FAILED in initialization!");
+    while (true)
+      ;
+  }
+  Serial.println("Re-Initialized!");
+
+  // Perform calibration sweep
+  if (!AD5933::calibrate(gain, phaseRef, reference, num_incr + 1)) {
+    Serial.println("Re-Calibration failed...");
+    while (true)
+      ;
+  }
+  Serial.println("Re-Calibrated!");
+
+}
+
+void selectPad(int p) {
+  // Pad 0 is calibration resistor, Pad 1-7 are on flex pcb
+  digitalWrite(sL[0], MUXtable[p][0]);
+  digitalWrite(sL[1], MUXtable[p][1]);
+  digitalWrite(sL[2], MUXtable[p][2]);
+}
---
+++ Interface/Impedance_8Mux/Impedance_8Mux.py
@@ -0,0 +1,131 @@
+from tkinter import *
+import tkinter as tk 
+from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
+import threading
+
+import serial
+from time import sleep
+import scipy
+import numpy as np
+from scipy.optimize import curve_fit
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (
+     FigureCanvasTkAgg)
+import matplotlib.animation as animation
+matplotlib.use('agg')
+
+
+commPort = '/dev/cu.usbmodem2101'
+ser = serial.Serial(commPort, baudrate = 9600)
+sleep(2)
+
+def changeSweepSettings():
+    ser.write(b'o')
+    global Start_Freq, Freq_Incr, Num_Incr, Ref_Res
+    startFreq = Start_Freq.get() + '\r'
+    freqIncr = Freq_Incr.get() + '\r'
+    numIncr = Num_Incr.get() + '\r'
+    refRes = Ref_Res.get() + '\r'
+    ser.write(startFreq.encode())
+    sleep(0.1)
+    ser.write(freqIncr.encode())
+    sleep(0.1)
+    ser.write(numIncr.encode())
+    sleep(0.1)
+    ser.write(refRes.encode())
+    sleep(0.1)
+    long_text = "Impedance Settings Changed:\nStart Frequency: " + startFreq.strip('\r') + "(Hz)" + "\nFrequency Increment: " + freqIncr.strip('\r') + " (Hz)" + "\nNumber of Increments: " + numIncr.strip('\r') + "\nReference Resistance: " + refRes.strip('\r') + " (Ohms)"
+    updateOutput(long_text)
+
+def frequencySweep():
+    ser.write(b'p')
+    frequency = [] 
+    impedance = []
+    i = 0
+    while True:
+        data = ser.readline().decode('ascii')
+        # print(data)
+        updateOutput(data)
+        if data.startswith('Frequency'):
+            break
+        # if i > 0:
+        #     impedance.append(float(data.strip(':').split('=')[1]))
+        #     frequency.append(int(data.split(':')[0])) 
+        # if data.startswith('Performing'):
+        #     i = 1
+
+def updateOutput(long):
+    OutputLabel.insert(tk.END, long)
+    OutputLabel.see('end')
+
+long_text = ""
+
+win = Tk() 
+frame1 = tk.Frame(win, relief=tk.RAISED, borderwidth=1)
+frame1.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+frame3 = tk.Frame(win, relief=tk.RAISED, borderwidth=1) 
+frame3.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
+
+# Dynamic Text Outputs
+o = tk.Label(frame3, text='Test Outputs')
+o.grid(column=0, row=0, sticky="nsew")
+
+OutputLabel = ScrolledText(frame3, width=60, height=30, wrap=tk.WORD, relief=tk.RAISED, borderwidth=1)
+OutputLabel.grid(column=0, row=1, sticky="nsew")
+
+OutputLabel.insert(tk.END, long_text)
+
+win.title('Impedance Measurement Interface')
+win.minsize(200,60)
+
+
+# Entry widgets
+def callback(P):
+    return str.isdigit(P) or P=='' or (str(P)[0] == '-' and str.isdigit(P[1:])) or str(P) == '-'
+vcmd = (win.register(callback))
+
+# Widgets
+
+calibrateImpedance = tk.Button(frame1, text='Calibrate Impedance', command=lambda : changeSweepSettings())
+calibrateImpedance.grid(row=5, column=1)
+
+impedanceSweep = tk.Button(frame1, text='Impedance Sweep', command=lambda : frequencySweep())
+impedanceSweep.grid(row=6, column=1)
+
+testButton = tk.Button(frame1, text='Test Resistance', command=lambda : frequencySweep())
+testButton.grid(row=8, column=1)
+
+# Entry
+
+Start_Freq = tk.Entry(frame1, bd=6, width=8, validate='key', validatecommand=(vcmd, '%P'))
+Freq_Incr = tk.Entry(frame1, bd=6, width=8, validate='key', validatecommand=(vcmd, '%P'))
+Num_Incr = tk.Entry(frame1, bd=6, width=8, validate='key', validatecommand=(vcmd, '%P'))
+Ref_Res = tk.Entry(frame1, bd=6, width=8, validate='key', validatecommand=(vcmd, '%P'))
+
+
+Start_Freq.insert(0, "10000")
+Freq_Incr.insert(0, "5000")
+Num_Incr.insert(0, "5")
+Ref_Res.insert(0, "270")
+
+Start_Freq.grid(column=1, row=0, sticky="nsew")
+Freq_Incr.grid(column=1, row=1, sticky="nsew")
+Num_Incr.grid(column=1, row=2, sticky="nsew")
+Ref_Res.grid(column=1, row=3, sticky="nsew")
+
+presStartLabel = tk.Label(frame1, text='Starting Frequency (kHz)')
+presIncrLabel = tk.Label(frame1, text='Frequency Increment (kHz)')
+presNumIncrLabel = tk.Label(frame1, text='Number of Increments')
+refResLabel = tk.Label(frame1, text='Reference Resistance (Ohms)')
+testResLabel = tk.Label(frame1, text='Test Resistance (Ohms)')
+
+presStartLabel.grid(column=0, row=0, sticky="nsew")
+presIncrLabel.grid(column=0, row=1, sticky="nsew")
+presNumIncrLabel.grid(column=0, row=2, sticky="nsew")
+refResLabel.grid(column=0, row=3, sticky="nsew")
+testResLabel.grid(column=0, row=7, sticky="nsew")
+
+win.mainloop()
