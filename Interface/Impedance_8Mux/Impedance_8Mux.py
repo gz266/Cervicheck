dca5954c64c27139080a44309064212c96316727
Neanth Suresh <ns976@cornell.edu>
2025-08-07 13:59:57 -0400

    Update Impedance_8Mux.py

--- Interface/Impedance_8Mux/Impedance_8Mux.py
+++ Interface/Impedance_8Mux/Impedance_8Mux.py
@@ -44,18 +44,70 @@ def frequencySweep():
     ser.write(b'p')
     frequency = [] 
     impedance = []
-    i = 0
+    phase = []
+    k = j.get()
     while True:
         data = ser.readline().decode('ascii')
         # print(data)
         updateOutput(data)
-        if data.startswith('Frequency'):
+        if data.startswith('Frequency sweep'):
             break
+        if data.startswith('Frequency'):
+            frequency.append(float(data.split(':')[1].strip()))
+        if data.startswith('Impedance'):
+            impedance.append(float(data.split(':')[1].strip()))
+        if data.startswith('Phase'):
+            phase.append(float(data.split(':')[1].strip()))
+    
         # if i > 0:
         #     impedance.append(float(data.strip(':').split('=')[1]))
         #     frequency.append(int(data.split(':')[0])) 
         # if data.startswith('Performing'):
         #     i = 1
+    if k == 1:
+        notebook.grid(column=1, row=0, sticky='NSEW')
+        win.grid_columnconfigure(2, weight=1)
+        notebook.grid_rowconfigure(0, weight=1)
+        notebook.grid_columnconfigure(0, weight=1)
+    fig_imp, ax_imp = plt.subplots(figsize=(3, 2), layout='constrained')
+    min_imp = min(impedance) - 1000 if min(impedance) - 1000 >= 0 else 0
+    max_imp = max(impedance) + 1000
+    ax_imp.set_ylim(min_imp, max_imp)                             # Set Y axis limit of plot
+    ax_imp.set_xlim(min(frequency), max(frequency))  
+    ax_imp.set_title("Impedance Bode Plot")                        # Set title of figure
+    ax_imp.set_ylabel("Impedance (Ohms)")                              # Set title of y axis 
+    ax_imp.set_xlabel("Frequency (Hz)")         # Set title of x axis
+    ax_imp.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(10))
+    ax_imp.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+    ax_imp.grid(True, which='both', linestyle='--', linewidth=0.5)
+    ax_imp.plot(frequency, impedance, 'o-', color='black')  # Plot impedance vs frequency
+    graph = tk.Frame()
+    canvas_imp = FigureCanvasTkAgg(fig_imp, master=graph)
+    canvas_imp.draw()
+    notebook.add(graph, text = 'Frequency Sweep ' + str(k))
+    graph.grid_rowconfigure(0, weight=1)
+    graph.grid_columnconfigure(0, weight=1)
+    graph.grid_columnconfigure(1, weight=1)
+    canvas_imp.get_tk_widget().grid(row=0, column=0, sticky='NSEW')
+
+    fig_phase, ax_phase = plt.subplots(figsize=(3, 2), layout='constrained')
+    ax_phase.set_ylim(-180, 180)
+    ax_phase.set_xlim(min(frequency), max(frequency))
+    ax_phase.set_title("Phase Bode Plot")                        # Set title of figure
+    ax_phase.set_ylabel("Phase (Degrees)")                              # Set title of y axis 
+    ax_phase.set_xlabel("Frequency (Hz)")         # Set title of x axis
+    ax_phase.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(10))
+    ax_phase.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+    ax_phase.grid(True, which='both', linestyle='--', linewidth=0.5)
+    ax_phase.plot(frequency, phase, 'o-', color='black')  # Plot phase vs frequency
+    canvas_phase = FigureCanvasTkAgg(fig_phase, master=graph)
+    canvas_phase.draw()
+    canvas_phase.get_tk_widget().grid(row=1, column=0, sticky='NSEW')
+
+    notebook.select(len(notebook.tabs())-1)
+    j.set(k + 1)
+    
+
 
 def selectPad():
     ser.write(b's')
@@ -72,6 +124,7 @@ def updateOutput(long):
 long_text = ""
 
 win = Tk() 
+j = tk.IntVar(value=1)
 frame1 = tk.Frame(win, relief=tk.RAISED, borderwidth=1)
 frame1.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 frame3 = tk.Frame(win, relief=tk.RAISED, borderwidth=1) 
@@ -121,7 +174,7 @@ Pad = tk.Entry(frame1, bd=6, width=8, validate='key', validatecommand=(vcmd, '%P
 Start_Freq.insert(0, "10000")
 Freq_Incr.insert(0, "5000")
 Num_Incr.insert(0, "5")
-Ref_Res.insert(0, "270")
+Ref_Res.insert(0, "300")
 Pad.insert(0, "0")
 
 Start_Freq.grid(column=1, row=0, sticky="nsew")
@@ -141,5 +194,8 @@ presIncrLabel.grid(column=0, row=1, sticky="nsew")
 presNumIncrLabel.grid(column=0, row=2, sticky="nsew")
 refResLabel.grid(column=0, row=3, sticky="nsew")
 testResLabel.grid(column=0, row=7, sticky="nsew")
+
+# Notebook for graphs (to be used when graph is actually produced)
+notebook = ttk.Notebook(win)
 
 win.mainloop()
