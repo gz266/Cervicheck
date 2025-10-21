756c95fa751a873c9d1c91653995fa3bd374ae02
Neanth Suresh <ns976@cornell.edu>
2025-10-21 16:43:19 -0400

    Update Master.py

--- Interface_Master/Master.py
+++ Interface_Master/Master.py
@@ -15,7 +15,7 @@ def main():
     ser = serial.Serial(commPort, baudrate = 9600)
     sleep(2)
 
-    strain = np.array([1.25, 1.35, 1.45, 1.55, 1.65, 1.75, 1.85])
+    strain = np.array([3.5, 3.6, 3.7, 3.8, 3.9, 4, 4.1])
 
     # Pandas dataframe to hold all data
     data = {'Pad number' : [1, 2, 3, 4, 5, 6, 7, 'α', 'C', 'Effective Modulus', 'Young\'s Modulus', 'Time (ms)']}
